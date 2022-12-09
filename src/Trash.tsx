54de7fa9530703c71bf6877f877f54b9c2d9aa9c
Sean <sosull@udel.edu>
2022-12-08 21:51:04 -0500

    Added trashcan

--- src/Trash.tsx
+++ src/Trash.tsx
@@ -1,6 +1,39 @@
-export {};
-//export function Trashcan() {
-//    return (
-//        export{}
-//    )
-//}
+import React from "react";
+import { useDragLayer, useDrop } from "react-dnd";
+import { ItemTypes } from "./constants";
+import { DragTile } from "./Interfaces/DragTile";
+
+export function Trashcan({
+    setBoxArray,
+    boxArray
+}: {
+    setBoxArray: React.Dispatch<React.SetStateAction<DragTile[]>>;
+    boxArray: DragTile[];
+}): JSX.Element {
+    const [{ item }, drop] = useDrop({
+        accept: ItemTypes.DragTile,
+        collect: (monitor) => ({
+            item: monitor.getItem(),
+            offset: monitor.getClientOffset()
+        }),
+        drop: () => {
+            const delArr = boxArray.filter((dt: DragTile) => {
+                return dt.id != item.data.id;
+            });
+            setBoxArray([...delArr]);
+        }
+    });
+    return (
+        <div
+            ref={drop}
+            style={{
+                height: "20%",
+                width: "20%",
+                backgroundColor: "#ababab",
+                position: "relative"
+            }}
+        >
+            Trash
+        </div>
+    );
+}
